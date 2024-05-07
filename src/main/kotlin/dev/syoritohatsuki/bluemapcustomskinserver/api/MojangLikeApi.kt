ad5ac853095990af580e90b591f5dbfb7f29b781
syorito-hatsuki <enderdron3@gmail.com>
2024-05-07 16:53:51 +0300

    Fixed MojangLikeApi file (Formater forget it)

--- src/main/kotlin/dev/syoritohatsuki/bluemapcustomskinserver/api/MojangLikeApi.kt
+++ src/main/kotlin/dev/syoritohatsuki/bluemapcustomskinserver/api/MojangLikeApi.kt
@@ -2,7 +2,6 @@ package dev.syoritohatsuki.bluemapcustomskinserver.api
 
 import dev.syoritohatsuki.bluemapcustomskinserver.BlueMapCustomSkinServerAddon.logger
 import dev.syoritohatsuki.bluemapcustomskinserver.config.ConfigManager
-import dev.syoritohatsuki.bluemapcustomskinserver.debugMessage
 import dev.syoritohatsuki.bluemapcustomskinserver.dto.mojang.Profile
 import dev.syoritohatsuki.bluemapcustomskinserver.dto.mojang.TextureInfo
 import kotlinx.coroutines.CoroutineScope
@@ -13,7 +12,6 @@ import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import java.awt.image.BufferedImage
 import java.net.URI
-import java.net.URL
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
@@ -28,7 +26,7 @@ class MojangLikeApi(private val uuid: UUID) {
             kotlin.runCatching {
                 json.decodeFromString<Profile>(withContext(Dispatchers.IO) {
                     URI.create(ConfigManager.read().url.replace("%uuid%", uuid.toString())).let {
-                        logger.debugMessage(it.toString())
+                        logger.debug(it.toString())
                         HttpClient.newHttpClient()
                             .send(HttpRequest.newBuilder(it).build(), HttpResponse.BodyHandlers.ofString()).body()
                     }
@@ -36,8 +34,8 @@ class MojangLikeApi(private val uuid: UUID) {
             }.onSuccess { profile ->
                 profile.properties.find { it.name == "textures" }?.let {
                     json.decodeFromString<TextureInfo>(String(Base64.getDecoder().decode(it.value))).apply {
-                        logger.debugMessage(textures.skin.url)
-                        complete(ImageIO.read(URL(textures.skin.url)))
+                        logger.debug(textures.skin.url)
+                        complete(ImageIO.read(URI(textures.skin.url).toURL().openStream()))
                     }
                 }
                 logger.info("Skin loaded successful")
